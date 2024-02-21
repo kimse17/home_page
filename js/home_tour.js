4e2fd4bf8bb3631573a6abd37a0fbb3fc039cf3a
kimse17 <seongeun1117@naver.com>
2024-02-21 16:40:04 +0900

    Create home_tour.js

---
+++ js/home_tour.js
@@ -0,0 +1,76 @@
+$(function () {
+    var item_width = $('.img_all>img').outerWidth(true);
+    $('.img_all img:first').before($('.img_all img:last'));
+    $('.img_all').css({
+        'left': -item_width + 'px'
+    });
+    $('.prev').click(function () {
+        var left_indent = parseInt($(".img_all").css('left')) + item_width;
+        $('.img_all').animate({
+            'left': left_indent + 'px'
+        }, 700, function () {
+            $('.img_all img:frist').before($('.img_all img:last'));
+            $('.img_all').css({
+                'left': -item_width + 'px'
+            });
+        });
+    });
+    $('.next').click(function () {
+        var left_indent = parseInt($('.img_all').css('left')) - item_width;
+        $('.img_all').animate({
+            'left': left_indent + 'px'
+        }, 700, function () {
+            $('.img_all img:last').after($('.img_all img:first'));
+            $('.img_all').css({
+                'left': -item_width + 'px'
+            });
+        });
+    });
+
+    function slide_ani() {
+        var left_indent = parseInt($('.img_all').css('left')) - item_width;
+        $('.img_all').stop().animate({
+            'left': left_indent + 'px'
+        }, 700, function () {
+            $('.img_all img:last').after($('.img_all img:first'));
+            $('.img_all').css('left', -item_width + 'px');
+        });
+    }
+    function stat_timer() {
+        timer = setInterval(slide_ani, 2000);
+    }
+    function stop_timer() {
+        clearInterval(timer);
+    }
+
+    //   $('.slide_img').hover(stop_timer, stat_timer);
+    stat_timer()
+})
+
+
+//  메뉴 마우스 호버
+$(function () {
+    $("#header_02").on("mouseover", function () {
+        $(".header_row_02_all").stop(true).animate({
+            height: "250px"
+        }, 200);
+    })
+    $("#header_02").on("mouseout", function () {
+        $(".header_row_02_all").stop(true).animate({
+            height: "0px"
+        }, 200);
+    })
+
+    $(".header_row_02_all").on("mouseover", function () {
+        $(".header_row_02_all").stop(true).animate({
+            height: "250px"
+        }, 200);
+    })
+    $(".header_row_02_all").on("mouseout", function () {
+        $(".header_row_02_all").stop(true).animate({
+            height: "0px"
+        }, 200);
+    })
+})
+
+
