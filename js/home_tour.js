28fdbf5171b3d167ea4554e0ae247aee79d7f01f
kimse17 <153714225+kimse17@users.noreply.github.com>
2024-05-27 16:03:02 +0900

    Update home_tour.js

--- js/home_tour.js
+++ js/home_tour.js
@@ -1,3 +1,31 @@
+AOS.init();
+$('.center').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
+
 $(function () {
     var item_width = $('.img_all>img').outerWidth(true);
     $('.img_all img:first').before($('.img_all img:last'));
